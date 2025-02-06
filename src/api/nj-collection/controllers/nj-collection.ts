714a7444b6e2a8d1253d3551650f9f122067de9b
niteshj11 <59691343+niteshj11@users.noreply.github.com>
2025-02-06 14:46:23 +0530

    first
    First commit

---
+++ src/api/nj-collection/controllers/nj-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * nj-collection controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::nj-collection.nj-collection');
---
+++ src/api/nj-collection/routes/nj-collection.ts
@@ -0,0 +1,7 @@
+/**
+ * nj-collection router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::nj-collection.nj-collection');
